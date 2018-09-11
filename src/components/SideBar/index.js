403eb7b9491ab1e6f45ae3dbf895a09f3616d30d
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2018-09-11 18:17:24 -0300

    Add estilos, form e lista de repositórios a sidebar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,11 +1,78 @@
-﻿import React from 'react';
+﻿import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
-import Container from './styles';
+import api from '../../services/api';
 
-const SideBar = () => (
-  <Container open>
-    <h1>Sidebar</h1>
-  </Container>
-);
+import { Container, Form, RepositoriesList } from './styles';
 
-export default SideBar;
+export default class Sidebar extends Component {
+  state = {
+    repositoryInput: '',
+    repositories: [],
+    selectedRepository: {},
+  }
+
+  handleRepositoryChange = (e) => {
+    this.setState({ repositoryInput: e.target.value });
+  }
+
+  handleRepositorySubmit = async (e) => {
+    e.preventDefault();
+
+    const { repositoryInput, repositories } = this.state;
+    const response = await api.get(`/repos/${repositoryInput}`);
+
+    this.setState({ repositories: [...repositories, response.data] });
+  }
+
+  handleSelectRepository = (repository) => {
+    this.setState({ selectedRepository: repository });
+
+    const { handleSelectedRepository } = this.props;
+
+    handleSelectedRepository(repository);
+  };
+
+  render() {
+    const { repositoryInput, repositories, selectedRepository } = this.state;
+    const { isOpen } = this.props;
+
+    return (
+      <Container isOpen={isOpen}>
+        <Form onSubmit={this.handleRepositorySubmit}>
+          <input
+            type="text"
+            placeholder="user/repository"
+            value={repositoryInput}
+            onChange={this.handleRepositoryChange}
+          />
+          <button type="submit">
+            <FontAwesomeIcon icon={faPlusCircle} color="#FFF" />
+          </button>
+        </Form>
+        <RepositoriesList>
+          {repositories.map(repository => (
+            <button
+              type="button"
+              selected={repository.id === selectedRepository.id ? 1 : 0}
+              key={repository.id}
+              onClick={() => this.handleSelectRepository(repository)}
+            >
+              <img src={repository.owner.avatar_url} alt={repository.full_name} />
+              <div>
+                <strong>
+                  {repository.name}
+                </strong>
+                <span>
+                  {repository.owner.login}
+                </span>
+              </div>
+              <FontAwesomeIcon icon={faAngleRight} color="#999" />
+            </button>
+          ))}
+        </RepositoriesList>
+      </Container>
+    );
+  }
+}
--- src/components/SideBar/styles.js
+++ src/components/SideBar/styles.js
@@ -1,11 +1,81 @@
 ﻿import styled from 'styled-components';
 
-const Container = styled.aside`
+export const Container = styled.aside`
   flex-shrink: 0;
-  background-color: #eee;
+  background-color: #FFF;
   overflow-x: hidden;
-  width: ${({ open }) => (open ? '250px' : '0')};
-  transition: width 0.4s ${({ open }) => (open ? 'ease-out' : 'ease-in')};
+  box-shadow: 5px 0px 25px 5px rgba(0,0,0,0.1);
+  z-index: 1;
+  width: ${({ isOpen }) => (isOpen ? '320px' : '0')};
+  padding: ${({ isOpen }) => (isOpen ? '30px' : '0')};
+  transition: width 0.4s ${({ isOpen }) => (isOpen ? 'ease-out' : 'ease-in')};
 `;
 
-export default Container;
+export const Form = styled.form`
+  display: flex;
+  border-bottom: 1px solid #EEE;
+  padding-bottom: 18px;
+
+  input {
+    background-color: #EEE;
+    height: 42px;
+    border-radius: 5px;
+    border: none;
+    width: 210px;
+    padding: 15px;
+  }
+  
+  button {
+    flex-grow: 1;
+    background-color: #59EA9A;
+    height: 42px;
+    font-size: 18px;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+    margin-left: 10px;
+  }
+`;
+
+export const RepositoriesList = styled.div`
+  button {
+    display: flex;
+    margin-top: 18px;
+    justify-content: start;
+    align-items: center;
+    opacity: ${({ selected }) => (selected ? 1 : 0.5)};
+    cursor: pointer;
+    background: none;
+    border: none;
+    width: 100%;
+
+    img {
+      height: 45px;
+      width: 45px;
+    }
+
+    div {
+      margin-left: 10px;
+      text-align: left;
+
+      strong {
+        display: block;
+        color: #333;
+        font-size: 16px;
+      }
+
+      span {
+        color: #666;
+        font-size: 14px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+    }
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+`;
