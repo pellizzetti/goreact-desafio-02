7773a38bdc4e114d7488a1279806d4c2378ccf61
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2018-09-11 18:17:42 -0300

    Add estilo e funcionalidade ao header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,65 @@
-﻿import React, { Fragment } from 'react';
+﻿import React, { Component, Fragment } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 import { Container, Header, Content } from './styles';
 
 import SideBar from '../../components/SideBar';
 
-const Home = () => (
-  <Fragment>
-    <SideBar />
-    <Container>
-      <Header>
-        <button type="button">Toggle</button>
-        <h1>Header</h1>
-      </Header>
-      <Content>
-        <h1>Content</h1>
-      </Content>
-    </Container>
-  </Fragment>
-);
+export default class Home extends Component {
+  state = {
+    selectedRepository: null,
+    isSideBarOpen: true,
+  }
 
-export default Home;
+  handleSelectedRepository = (repository) => {
+    this.setState({ selectedRepository: repository });
+  }
+
+  handleSideBarStatus = () => {
+    const { isSideBarOpen } = this.state;
+    const sideBarOpen = !isSideBarOpen;
+    this.setState({ isSideBarOpen: sideBarOpen });
+  }
+
+  render() {
+    const { selectedRepository, isSideBarOpen } = this.state;
+
+    return (
+      <Fragment>
+        <SideBar
+          handleSelectedRepository={this.handleSelectedRepository}
+          isOpen={isSideBarOpen}
+        />
+        <Container>
+          <Header>
+            <FontAwesomeIcon
+              icon={isSideBarOpen ? faAngleLeft : faAngleRight}
+              color="#999"
+              onClick={this.handleSideBarStatus}
+            />
+            {selectedRepository && (
+              <Fragment>
+                <img src={selectedRepository.owner.avatar_url} alt={selectedRepository.full_name} />
+                <div>
+                  <strong>
+                    {selectedRepository.name}
+                  </strong>
+                  <span>
+                    {selectedRepository.owner.login}
+                  </span>
+                </div>
+                <select>
+                  <option value="Abertas">Abertas</option>
+                </select>
+              </Fragment>
+            )}
+          </Header>
+          <Content>
+            <h1>Content</h1>
+          </Content>
+        </Container>
+      </Fragment>
+    );
+  }
+}
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -11,11 +11,48 @@ export const Header = styled.header`
   display: flex;
   flex-direction: row;
   align-items: center;
-  height: 64px;
-  padding-left: 20px;
-  background-color: #38d;
+  height: 100px;
+  padding: 30px;
+  background-color: #FFF;
+
+  svg {
+    width: 1.5em !important;
+    height: 100%;
+    cursor: pointer;
+  }
+
+  img {
+    width: 45px;
+    height: 45px;
+    margin-left: 10px;
+  }
+
+  div {
+    margin-left: 10px;
+    text-align: left;
+    flex: 1;
+
+    strong {
+      display: block;
+      color: #333;
+      font-size: 16px;
+    }
+
+    span {
+      color: #666;
+      font-size: 14px;
+    }
+  }
+
+  select {
+    border: 1px solid #DDD;
+    padding: 0 15px;
+    height: 42px;
+    flex-grow: 0.2;
+  }
 `;
 
 export const Content = styled(Container)`
+  background-color: #F5F5F5;
   padding: 20px;
 `;
